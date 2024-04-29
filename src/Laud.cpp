771cdd5a07c5a4b786f5331f8f76716d3f4512df
MartinPuskin <martin.puskin@gmail.com>
2024-04-29 17:04:15 +0300

    Lisasin Sõna x2 ja Sõna x3 Lauale. Muutsin Künas muutujate nimesid ja nüüd üks muutuja näitab maksimaalset tähtede sisalduvust.

--- src/Laud.cpp
+++ src/Laud.cpp
@@ -4,11 +4,12 @@
 
 #include <iostream>
 #include "Laud.h"
-//testcomment
 
 set<short> Laud::m_kahekordsedTahed = {3, 11, 36, 38, 45, 52, 59, 92, 96, 98, 102, 108, 116, 122, 126, 128, 132, 165,
                                        172, 179, 186, 188, 213, 221};
 set<short> Laud::m_kolmekordsedTahed = {20, 24, 76, 80, 84, 88, 136, 140, 144, 148, 200, 204};
+set<short> Laud::m_kahekordsedSonad = {16,28,32,42,48,56,64,70,154,160,168,176,182,192,196,208};
+set<short> Laud::m_kolmekordsedSonad = {0,14,105,119,210,224};
 
 
 Laud::Laud() {
@@ -19,6 +20,14 @@ Laud::Laud() {
         }
         if (m_kolmekordsedTahed.contains(i)){
             m_mangulaud.push_back(make_shared<Ruut>(1,3));
+            continue;
+        }
+        if (m_kahekordsedSonad.contains(i)){
+            m_mangulaud.push_back(make_shared<Ruut>(2,1));
+            continue;
+        }
+        if (m_kolmekordsedSonad.contains(i)){
+            m_mangulaud.push_back(make_shared<Ruut>(3,1));
             continue;
         }
         m_mangulaud.push_back(make_shared<Ruut>(1,1));
